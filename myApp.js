1331827d0d4284ff79d11a671ac88bf8d6c3d61a
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2024-02-15 14:15:39 +0100

    task7

--- myApp.js
+++ myApp.js
@@ -40,7 +40,12 @@ app.get('/', function (req, res) {
 
 
 
-
+  app.get('/now', function(req, res, next) {
+    req.time = (new Date()).toString();
+    next();
+  }, function(req, res) {
+    res.json({time: req.time});
+  });
 
 
 
